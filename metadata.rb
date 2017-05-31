62d238e711f994890e2142d05dda0db28c589739
Marcin Wolny <marcin@wutanic.com>
2017-05-31 18:06:39 +0200

    Initial support for chef 13

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,16 @@ maintainer_email 'team@devops.dance'
 license 'Apache-2.0'
 description 'Installs and configures tinyproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
-chef_version '~> 12'
+version '1.1.0'
 issues_url 'https://github.com/DevopsDance/chef-cookbook-tinyproxy/issues'
 source_url 'https://github.com/DevopsDance/chef-cookbook-tinyproxy'
 
 depends 'ubuntu'
 depends 'apt'
 depends 'chef-sugar'
+
+chef_version '~> 12'
+chef_version '~> 13'
 
 supports 'ubuntu', '>= 15.04'
 supports 'debian', '>= 7.0'
